4e706cef131e8322dc367c3d910a7c58a8ea688a
MustafaGrgn <mustafagorgun87@hotmail.com>
2021-10-25 07:28:35 +0300

    Initial Commit

---
+++ HW5_Mustafa_Gorgun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW5_Mustafa_Gorgun
+//
+//  Created by MstfGrgn on 25.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
